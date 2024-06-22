d8c8829a179c3e22a1584e35b3d35c93163e3a09
Julio Cesar <juliocesarmatosvirgens@gmail.com>
2024-06-22 16:09:50 -0300

    aumentando dificuldade

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 let computerNumber;
 let userNumbers = [];
 let attempts = 0;
-const maxAttempts = 10;
+let maxAttempts = 10;
 let victories = 0;
 const maxVictories = 20;
+let minRange = 1;
+let maxRange = 100;
 
 function newGame() {
     window.location.reload();
 }
 
 function init() {
-    computerNumber = Math.floor(Math.random() * 100 + 1);
+    computerNumber = generateRandomNumber(minRange, maxRange);
     console.log(computerNumber);
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function compareNumbers() {
@@ -28,6 +34,19 @@ function compareNumbers() {
             victories++;
             document.getElementById('textOutput').innerHTML = 'Parabéns! Você acertou o número!';
             document.getElementById('inputBox').setAttribute('readonly', 'readonly');
+
+            // Ajusta o intervalo para o próximo jogo
+            minRange = computerNumber - 20;
+            maxRange = computerNumber + 20;
+
+            // Atualiza o máximo de tentativas baseado na vitória
+            maxAttempts = 10 - victories;
+
+            // Atualiza o máximo de tentativas exibido
+            document.getElementById('maxAttempts').innerHTML = maxAttempts;
+
+            // Reinicia as tentativas para o próximo jogo
+            attempts = 0;
         }
         attempts++;
         document.getElementById('attempts').innerHTML = attempts;
